7454fe08cf8c87da3cbbe120f968161847cca015
Matt Rooney <matthewjrooney@gmail.com>
2016-03-02 11:34:16 -0700

    end of tutorial - logging votes to both consoles

--- server.js
+++ server.js
@@ -29,20 +29,33 @@ io.on('connection', function (socket) {
 
   socket.emit('statusMessage', 'You have connected.');
 
-  socket.on('disconnect', function () {
-    console.log('A user has disconnected.', io.engine.clientsCount);
-    delete votes[socket.id];
-    console.log(votes);
-    io.sockets.emit('userConnection', io.engine.clientsCount);
-  });
-
   socket.on('message', function (channel, message) {
     if (channel === 'voteCast') {
       votes[socket.id] = message;
-      console.log(votes);
+      socket.emit('voteCount', countVotes(votes));
     }
   });
 
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+    delete votes[socket.id];
+    socket.emit('voteCount', countVotes(votes));
+    io.sockets.emit('userConnection', io.engine.clientsCount);
+  });
+
 });
+
+function countVotes(votes) {
+  var voteCount = {
+    A: 0,
+    B: 0,
+    C: 0,
+    D: 0
+  };
+  for (var vote in votes) {
+    voteCount[votes[vote]]++
+  }
+  return voteCount;
+}
 
 module.exports = server;
