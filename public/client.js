789c33fd7e988ffc289537dafc2246c2e33f3001
Matt Rooney <matthewjrooney@gmail.com>
2016-03-02 15:04:50 -0700

    add vote emitter and chart

--- public/client.js
+++ public/client.js
@@ -2,6 +2,16 @@ var socket = io();
 
 var connectionCount = document.getElementById('connection-count');
 var statusMessage = document.getElementById('status-message');
+var currentPoll = document.getElementById('vote-count');
+var yourVote = document.getElementById('your-vote');
+
+var buttons = document.querySelectorAll('#choices button');
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    socket.send('voteCast', this.innerText);
+  });
+}
 
 socket.on('usersConnected', function (count) {
   connectionCount.innerText = 'Connected Users: ' + count;
@@ -12,13 +22,24 @@ socket.on('statusMessage', function (message) {
 });
 
 socket.on('voteCount', function (votes) {
-  console.log(votes);
+  currentPoll.innerText = ['A: ' + votes.A,
+                           ' B: ' + votes.B,
+                           ' C: ' + votes.C,
+                           ' D: ' + votes.D]
+
+   var data = {
+     // A labels array that can contain any sort of values
+     labels: ['A', 'B', 'C', 'D'],
+     // Our series array that contains series objects or in this case series data arrays
+     series: [
+       [votes.A, votes.B, votes.C, votes.D]
+     ]
+   };
+
+   new Chartist.Line('.ct-chart', data);
+
 });
 
-var buttons = document.querySelectorAll('#choices button');
-
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function () {
-    socket.send('voteCast', this.innerText);
-  });
-}
+socket.on('currentVote' ,function(vote) {
+  yourVote.innerText = 'You voted for ' + vote;
+})
